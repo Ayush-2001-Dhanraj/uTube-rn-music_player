41cd83634b1fd5686f804783824a0b012b81e843
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2025-01-22 22:54:46 +0530

    setup music player service

---
+++ src/services/musicPlayerService.js
@@ -0,0 +1,35 @@
+import TrackPlayer, {Event, RepeatMode} from 'react-native-track-player';
+
+import {playListData} from '../constants';
+
+export const playBackService = async () => {
+  TrackPlayer.addEventListener(Event.RemotePause, () => {
+    TrackPlayer.pause();
+  });
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    TrackPlayer.play();
+  });
+  TrackPlayer.addEventListener(Event.RemoteNext, () => {
+    TrackPlayer.skipToNext();
+  });
+  TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+    TrackPlayer.skipToPrevious();
+  });
+};
+
+export const setupPlayer = async () => {
+  let isSetup = false;
+  try {
+    await TrackPlayer.getActiveTrackIndex();
+    isSetup = true;
+  } catch (error) {
+    await TrackPlayer.setupPlayer();
+  } finally {
+    return isSetup;
+  }
+};
+
+export const addTracks = async () => {
+  await TrackPlayer.add(playListData);
+  await TrackPlayer.setRepeatMode(RepeatMode.Queue);
+};
